d6553a558a7d17c591ac25d2a2d733020cba8d5b
desa101 <88065447+desa101@users.noreply.github.com>
2021-09-13 12:54:27 +0800

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Power{
+	public static void main(String args [])
+{
+	Scanner input= new Scanner(System.in);
+
+
+        
+	
+
+	System.out.println(" Enter value of a number  for a: ");
+        double a = input.nextDouble();
+
+System.out.println(" Enter value of a number  : ");
+        double b = input.nextDouble();
+
+System.out.println(" Enter value of a number : ");
+        double c = input.nextDouble();
+double d = Math.pow(b,c);
+double  e =  Math.pow(a, d) ;
+
+
+
+System.out.println("The Floor is : " + e);
+	}
+
+}
